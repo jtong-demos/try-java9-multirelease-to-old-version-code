37f772daf549c71491e9861c4c297592353f64a8
Jian Tong <tj19832@gmail.com>
2018-08-22 00:28:01 +0800

    try release 6

---
+++ multirelease-pure-code/README.md
@@ -0,0 +1,6 @@
+support as far as java 6
+
+```s
+cd src
+javac --release 6  com/thoughtworks/Java9Code.java
+```
---
+++ multirelease-pure-code/src/com/thoughtworks/Java9Code.java
@@ -0,0 +1,7 @@
+package com.thoughtworks;
+
+public class Java9Code{
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
